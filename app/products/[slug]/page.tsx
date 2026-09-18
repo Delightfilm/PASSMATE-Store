91cdfbacbac0f0a384dbb0595712bea785849751
Delightfilm <jhpodong000000@gmail.com>
2026-09-18 13:45:43 +0900

    build: generate static product route

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -3,6 +3,10 @@ import Link from "next/link";
 import { ProductCover } from "@/components/product-cover";
 import { getProduct } from "@/lib/products";
 
+export function generateStaticParams() {
+  return [{ slug: "computer-literacy-2" }];
+}
+
 export default async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
   const product = getProduct(slug);
